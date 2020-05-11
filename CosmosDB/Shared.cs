3cd7bdb927395ec340a9df11c4cdd44fc2fd6d3d
Donnis94 <36189129+Donnis94@users.noreply.github.com>
2020-05-11 12:56:18 +0200

    Add files via upload

---
+++ CosmosDB/Shared.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
+
+public class Shared
+{
+	public static CosmosClient Client { get; private set; }
+	static Shared()
+	{
+		var endpoint = "https://exjobb2020.documents.azure.com:443/";
+		var masterKey = "PWWJrJH9llc2YFY8WMUNZebljTjrkbdI1kiTESvBNm9VVpdNVJjbwPAzEOnwXrUVIm4Vthim4XvTEcMUcIdUaw==";
+		Client = new CosmosClient(endpoint, masterKey);
+	}
+}
