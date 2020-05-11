85c8aefe173561bd1fe59fc5f0cf15402ba640b2
Donnis94 <36189129+Donnis94@users.noreply.github.com>
2020-05-11 13:04:06 +0200

    Update Shared.cs

--- Elasticsearch/Shared.cs
+++ Elasticsearch/Shared.cs
@@ -8,7 +8,7 @@ namespace elasticsearch
     public class Shared
     {
 		public static ElasticClient Client { get; private set; }
-		private const string ElasticUri = "https://elastic:tGuN5QwaqyrTt79lFvaPIrA5@9d7877f17a72445ca79a84a5cea45f3f.eastus2.azure.elastic-cloud.com:9243";
+		private const string ElasticUri = "https://{username}:{password}{elasticsearch endpoint}";
 
 		static Shared(){
 			var uri = new Uri(ElasticUri);
